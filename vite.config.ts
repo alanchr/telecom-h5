2ab1fdbdd26ae052afda999459cd952b27a2fa43
alanchen <2451023133@qq.com>
2026-07-08 17:07:35 +0800

    fix: correct vite import path from 'vite/config' to 'vite'

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
